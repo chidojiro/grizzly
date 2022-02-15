aa396b0219facbff45bc7a2296f8410f08eb630a
Nick <chidojiro@gmail.com>
2022-02-15 18:39:39 +0700

    fix: special order item

--- src/components/Main/Results/Item/Availability/Availability.tsx
+++ src/components/Main/Results/Item/Availability/Availability.tsx
@@ -1,27 +1,28 @@
 import { SearchResult } from 'types';
 import tw from 'twin.macro';
+import isEqual from 'lodash/isEqual';
 
 export type Props = {
   data: SearchResult['values'];
 };
 
-export const Availability = ({ data: { instock, quantityavailable } }: Props) => {
+export const Availability = ({ data: { instock, quantityavailable, special_order } }: Props) => {
   let color = tw``;
   let background = tw``;
   let label = '';
   let padding = tw``;
 
-  if (instock === 'true') {
-    color = tw`text-green`;
-    label = 'Ready To Ship';
-  } else if (quantityavailable === '0') {
-    color = tw`text-[#ae0101]`;
-    label = 'More on the Way';
-  } else {
+  if (isEqual(special_order, ['1'])) {
     color = tw`text-black`;
     label = 'Special Order Item';
     background = tw`bg-yellow`;
     padding = tw`px-[10px] py-[5px]`;
+  } else if (quantityavailable === '0') {
+    color = tw`text-[#ae0101]`;
+    label = 'More on the Way';
+  } else if (instock === 'true') {
+    color = tw`text-green`;
+    label = 'Ready To Ship';
   }
 
   if (!label) return null;
